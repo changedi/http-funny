805ba83b3a4878a406fc401b44209bf020b4534c
changedi <jysoftware@gmail.com>
2015-12-01 17:23:34 +0800

    test exception/HttpConfigException.java
    Signed-off-by: changedi <jysoftware@gmail.com>

--- src/main/java/com/github/changedi/http/exception/HttpConfigException.java
+++ src/main/java/com/github/changedi/http/exception/HttpConfigException.java
@@ -2,9 +2,12 @@ package com.github.changedi.http.exception;
 
 public class HttpConfigException extends Exception {
 
-	/**
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = 7952317814183875992L;
+    private static final long serialVersionUID = 7952317814183875992L;
 
+    public String toString() {
+        return "[Exception]:" + this.getClass().getName();
+    }
 }
